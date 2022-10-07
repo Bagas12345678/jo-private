6c7348a52400869422abd4ae8c18ab2e652f55c5
Bagas12345678 <106974492+Bagas12345678@users.noreply.github.com>
2022-10-07 20:59:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,15 +17,15 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'BAGAS MAKER'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
-global.author = 'WhatsApp Bot'
+global.owner = ['6285892778812','6285892778812','6285892778812']
+global.premium = ['6285892778812']
+global.packname = 'BAGAS MAKER'
+global.author = 'BAGAS MAKER'
 global.wlcm = []
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
